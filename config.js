1c08b9f06343df48145d1db39382e59923b68474
Divyesh Parmar <divyeshpar2496@gmail.com>
2018-06-05 10:36:34 +0530

    configs shouldn't be visible

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: 'G10GQm8eeFoS5vTqkA7ACV9Ef',
-    consumer_secret: '1Jz69MRgzOPzDmpb95Shigo20FR0qU2rzVpqBbpRsNRwa5Ebho',
-    access_token_key: '2323246538-5qsi050mhjgEllPQq1KhNbR7867MtWnuUFGMdHf',
-    access_token_secret: 'gk0MXuNjBxsIFmRnDX5clDPhEDmo98f6wPcMU2OrNu4P3'
-}
